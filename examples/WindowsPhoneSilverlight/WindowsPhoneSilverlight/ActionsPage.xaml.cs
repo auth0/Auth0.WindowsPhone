101299c2a2c92b0e4af896ccfb1da6a86a0a160f
dschenkelman <damian@auth0.com>
2014-08-11 08:18:29 -0700

    Added windows phone silverlight sample

---
+++ examples/WindowsPhoneSilverlight/WindowsPhoneSilverlight/ActionsPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Net.Http;
+using System.Threading;
+using System.Net.Http.Headers;
+
+namespace WindowsPhoneSilverlight
+{
+    public partial class ActionsPage : PhoneApplicationPage
+    {
+        private string idToken;
+
+        public ActionsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        
+            string idToken;
+
+            if (!NavigationContext.QueryString.TryGetValue("id_token", out idToken))
+            {
+                throw new InvalidOperationException("Navigated without id_token parameter");
+            }
+
+            this.idToken = idToken;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.idToken);                
+
+            // using online sample to workaround emulator limitations accesing host
+            var responseMessage = await client.GetAsync(
+                new Uri("http://auth0-aspnet-webapi-owin.azurewebsites.net/secured/ping", UriKind.Absolute),
+                CancellationToken.None);
+
+            var content = await responseMessage.Content.ReadAsStringAsync();
+        
+            MessageBox.Show(content);
+        }
+    }
+}
---
+++ examples/WindowsPhoneSilverlight/WindowsPhoneSilverlight/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Auth0.SDK;
+using Microsoft.Phone.Controls;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WindowsPhoneSilverlight
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private Auth0Client auth0Client = new Auth0Client("samples.auth0.com", "BUIJSW9x60sIHBw8Kd9EmCbj8eDIFxDC"); 
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginClick(object sender, RoutedEventArgs args)
+        {
+            try
+            {
+                var user = await auth0Client.LoginAsync();
+                NavigationService.Navigate(new Uri("/ActionsPage.xaml?id_token=" + user.IdToken, UriKind.Relative));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return;
+            }
+       }
+    }
+}
